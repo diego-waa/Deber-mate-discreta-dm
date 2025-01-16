9bfbce97c010b978032099a30cd75520f8370280
diego-waa <diegomolina2904@hotmail.com>
2025-01-15 19:14:05 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,13 @@
+// funciones.h
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <stdio.h>
+#include <string.h>
+
+// Prototipos de funciones
+void invertirCadena(char *cadena, int inicio, int fin);
+unsigned long long calcularFactorial(int numero);
+void generarFibonacci(int limite);
+
+#endif
